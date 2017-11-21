92643a5b3cbd4da3ede10d1812cd2c5e79af7f87
AbdelRahman <abdelrhmanalaa39@gmail.com>
2017-11-21 17:09:01 +0000

    bugs

--- handler/index.js
+++ handler/index.js
@@ -3,7 +3,7 @@ const User = require('./models/user');
 const crypto = require('crypto');
 const algorithm = 'aes-256-ctr';
 const password = 'd6F3Efeq';
-
+const mongoose = require('mongoose');
 class FacebookCallbackHandler {
     constructor(event) {
         this.event = event;
@@ -95,21 +95,21 @@ class StudentService {
               user.save();
             }
           }
-        })
+        });
         }
     }
 }
 
 function encrypt(text){
-  var cipher = crypto.createCipher(algorithm,password)
-  var crypted = cipher.update(text,'utf8','hex')
+  var cipher = crypto.createCipher(algorithm,password);
+  var crypted = cipher.update(text,'utf8','hex');
   crypted += cipher.final('hex');
   return crypted;
 }
  
 function decrypt(text){
-  var decipher = crypto.createDecipher(algorithm,password)
-  var dec = decipher.update(text,'hex','utf8')
+  var decipher = crypto.createDecipher(algorithm,password);
+  var dec = decipher.update(text,'hex','utf8');
   dec += decipher.final('utf8');
   return dec;
 }
