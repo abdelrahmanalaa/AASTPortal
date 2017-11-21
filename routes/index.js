60fe409624733148bc75a2f9d4e472f4e03d797b
AbdelRahman <abdelrhmanalaa39@gmail.com>
2017-11-21 18:51:01 +0000

    hello

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,10 @@ router.get("/webhook", function (req, res) {
 
 router.post("/webhook", function (req, res) {
   if (req.body.object == "page") {
-    console.log(req.body.entry);
+    
     req.body.entry.forEach(function(entry) {
       entry.messaging.forEach(function(event) {
+        console.log(event);
         var facebookHandler = new FacebookCallbackHandler(event);
         if (event.postback) {
           facebookHandler.processPostback();
